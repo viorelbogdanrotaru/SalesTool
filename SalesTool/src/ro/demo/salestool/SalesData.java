685f4fcb7bd4eb873137897e335d06fc6ee2a7c4
viorel.rotaru <viorel.rotaru@STSBU-01W046.star.ro>
2016-08-03 17:14:57 +0300

    Partial implementation

--- SalesTool/src/ro/demo/salestool/SalesData.java
+++ SalesTool/src/ro/demo/salestool/SalesData.java
@@ -2,4 +2,9 @@ package ro.demo.salestool;
 
 public class SalesData {
 
+	int[] data = {0, 4, 9};
+	
+	public void displayData() {
+		System.out.println("Temporar: " + data[0] + " " + data[1] + " " + data[2]);
+	}
 }
