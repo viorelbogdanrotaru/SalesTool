1d4520c6fecffbaa8a119e439528891d4b8e3717
viorel.rotaru <viorel.rotaru@star-storage.ro>
2016-08-03 17:33:38 +0300

    Final display sales data !!!

--- SalesTool/src/ro/demo/salestool/SalesData.java
+++ SalesTool/src/ro/demo/salestool/SalesData.java
@@ -5,6 +5,9 @@ public class SalesData {
 	int[] data = {0, 4, 9};
 	
 	public void displayData() {
-		System.out.println("Temporar: " + data[0] + " " + data[1] + " " + data[2]);
+		System.out.println("");
+		for (int i : data) {
+			System.out.println("Next value: " + i);
+		}
 	}
 }
